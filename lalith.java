bbfe67726505536d7ecf999b1e29f033e0eb734e
Lalith808 <lalithkishore088@gmail.com>
2024-07-09 21:46:15 +0530

    Add files via upload

---
+++ lalith.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class sdf{
+   sdf(){
+    System.out.println("heyy");
+  }
+}
+class lalith{
+  public static void main(String ad[]){
+            
+sdf sdf1 = new sdf();
+
+
+ }
+}
